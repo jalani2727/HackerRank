232870e833a8a240be9bb3639ee8868de2e1921a
Jalani Paul <jalani2727@gmail.com>
2019-06-20 14:26:09 -0400

    left rotation solution

---
+++ left_rotation.py
@@ -0,0 +1,15 @@
+import unittest
+
+def rotLeft(a, d):
+    i = 0
+    new_list = [num for num in range(0,len(a))]
+    for num in a:
+        new_list[i-d]=a[i]
+        i+=1
+    return new_list
+
+
+class MyTests(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(rotLeft([33,47,70,37,8,53,13,93,71,72,51,100,60,87,97], 13))
+   
